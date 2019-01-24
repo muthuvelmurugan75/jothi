7a3caa02011dc3948688539d2e78f436f1913904
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2019-01-24 17:25:07 +0000

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,19 @@
+package servlet2;
+
+public class Method {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Method obj=new Method();
+int value=obj.add(5, 6);
+System.out.println();
+	}
+
+	public int add(int a,int b){
+	int c=a+b;
+		return c;
+	}
+}
