68dfe51f061559f37651cd87808cb02cbc061609
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2019-02-21 18:08:52 +0000

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,21 @@
+package pppp;
+
+public class Sum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=12;int r=0; int s=0;
+while(n>0){
+	r=n%10;
+	s=s+r;
+	n=n/10;
+			
+			
+}
+System.out.println(s);
+	}
+
+}
